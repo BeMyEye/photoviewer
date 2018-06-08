9c1014de5e870c21b8f9f25299cfa0ae23b277a9
PapyElGringo <adrien.peslerbe@bemyeye.com>
2018-06-08 15:24:29 +0200

    fix dataurl

--- src/android/PhotoActivity.java
+++ src/android/PhotoActivity.java
@@ -1,12 +1,12 @@
 package com.sarriaroman.PhotoViewer;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
-
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Base64;
@@ -16,252 +16,275 @@ import android.widget.ImageView;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Request;
+import com.squareup.picasso.RequestHandler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import android.support.media.ExifInterface;
+
+
 public class PhotoActivity extends Activity {
-    private PhotoViewAttacher mAttacher;
+	private PhotoViewAttacher mAttacher;
 
-    private ImageView photo;
+	private ImageView photo;
+	private String imageUrl;
 
-    private ImageButton closeBtn;
-    private ImageButton shareBtn;
-    private ProgressBar loadingBar;
+	private ImageButton closeBtn;
+	private ImageButton shareBtn;
 
-    private TextView titleTxt;
+	private TextView titleTxt;
 
-    private String mImage;
-    private String mTitle;
-    private JSONObject mOptions;
-    private File mTempImage;
-    private int shareBtnVisibility;
+	private JSONObject options;
+	private int shareBtnVisibility;
 
-    public static JSONArray mArgs = null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		setContentView(getApplication().getResources().getIdentifier("activity_photo", "layout", getApplication().getPackageName()));
 
-        setContentView(getApplication().getResources().getIdentifier("activity_photo", "layout", getApplication().getPackageName()));
+		// Load the Views
+		findViews();
 
-        // Load the Views
-        findViews();
+		try {
+			options = new JSONObject(this.getIntent().getStringExtra("options"));
+			shareBtnVisibility = options.getBoolean("share") ? View.VISIBLE : View.INVISIBLE;
+		} catch(JSONException exception) {
+			shareBtnVisibility = View.VISIBLE;
+		}
+		shareBtn.setVisibility(shareBtnVisibility);
 
-        try {
-            this.mImage = mArgs.getString(0);
-            this.mTitle = mArgs.getString(1);
-            this.mOptions = mArgs.getJSONObject(2);
-            //Set the share button visibility
-            shareBtnVisibility = mOptions.getBoolean("share") ? View.VISIBLE : View.INVISIBLE;
+		// Change the Activity Title
+		String actTitle = this.getIntent().getStringExtra("title");
+		if( !actTitle.equals("") ) {
+			titleTxt.setText(actTitle);
+		}
+
+		// Set Button Listeners
+		closeBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+		shareBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Uri bmpUri = getLocalBitmapUri(photo);
+
+				if (bmpUri != null) {
+				    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+				    sharingIntent.setType("image/*");
+				    sharingIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+
+				    startActivity(Intent.createChooser(sharingIntent, "Share"));
+				}
+			}
+		});
 
 
-        } catch (JSONException exception) {
-            shareBtnVisibility = View.VISIBLE;
-        }
-        shareBtn.setVisibility(shareBtnVisibility);
-        //Change the activity title
-        if (!mTitle.equals("")) {
-            titleTxt.setText(mTitle);
-        }
+		loadImage(PhotoViewer.url);
+	}
 
-        loadImage();
+	/**
+	 * Find and Connect Views
+	 *
+	 */
+	private void findViews() {
+		// Buttons first
+		closeBtn = (ImageButton) findViewById( getApplication().getResources().getIdentifier("closeBtn", "id", getApplication().getPackageName()) );
+		shareBtn = (ImageButton) findViewById( getApplication().getResources().getIdentifier("shareBtn", "id", getApplication().getPackageName()) );
 
-        // Set Button Listeners
-        closeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+		// Photo Container
+		photo = (ImageView) findViewById( getApplication().getResources().getIdentifier("photoView", "id", getApplication().getPackageName()) );
+		mAttacher = new PhotoViewAttacher(photo);
 
-        shareBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri imageUri;
-                if (mTempImage == null){
-                    mTempImage = getLocalBitmapFileFromView(photo);
-                }
+		// Title TextView
+		titleTxt = (TextView) findViewById( getApplication().getResources().getIdentifier("titleTxt", "id", getApplication().getPackageName()) );
+	}
 
-                imageUri = Uri.fromFile(mTempImage);
+	/**
+	 * Get the current Activity
+	 *
+	 * @return
+	 */
+	private Activity getActivity() {
+		return this;
+	}
 
-                if (imageUri != null) {
-                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+	/**
+	 * Hide Loading when showing the photo. Update the PhotoView Attacher
+	 */
+	private void hideLoadingAndUpdate() {
+		photo.setVisibility(View.VISIBLE);
 
-                    sharingIntent.setType("image/*");
-                    sharingIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-
-                    startActivity(Intent.createChooser(sharingIntent, "Share"));
-                }
-            }
-        });
-
-    }
-
-    /**
-     * Find and Connect Views
-     */
-    private void findViews() {
-        // Buttons first
-        closeBtn = (ImageButton) findViewById(getApplication().getResources().getIdentifier("closeBtn", "id", getApplication().getPackageName()));
-        shareBtn = (ImageButton) findViewById(getApplication().getResources().getIdentifier("shareBtn", "id", getApplication().getPackageName()));
-
-        //ProgressBar
-        loadingBar = (ProgressBar) findViewById(getApplication().getResources().getIdentifier("loadingBar", "id", getApplication().getPackageName()));
-        // Photo Container
-        photo = (ImageView) findViewById(getApplication().getResources().getIdentifier("photoView", "id", getApplication().getPackageName()));
-        mAttacher = new PhotoViewAttacher(photo);
-
-        // Title TextView
-        titleTxt = (TextView) findViewById(getApplication().getResources().getIdentifier("titleTxt", "id", getApplication().getPackageName()));
-    }
-
-    /**
-     * Get the current Activity
-     *
-     * @return
-     */
-    private Activity getActivity() {
-        return this;
-    }
-
-    /**
-     * Hide Loading when showing the photo. Update the PhotoView Attacher
-     */
-    private void hideLoadingAndUpdate() {
-        photo.setVisibility(View.VISIBLE);
-        loadingBar.setVisibility(View.INVISIBLE);
         shareBtn.setVisibility(shareBtnVisibility);
 
-        mAttacher.update();
-    }
+		mAttacher.update();
+	}
 
-    /**
-     * Load the image using Picasso
-     */
-    private void loadImage() {
-        if (mImage.startsWith("http") || mImage.startsWith("file")) {
-            Picasso.with(this)
-                    .load(mImage)
-                    .fit()
-                    .centerInside()
-                    .into(photo, new com.squareup.picasso.Callback() {
-                        @Override
-                        public void onSuccess() {
-                            hideLoadingAndUpdate();
-                        }
+	/**
+	 * Load the image using Picasso
+	 *
+	 */
+	private void loadImage(String url) {
+		// create Picasso.Builder object
+		Picasso.Builder picassoBuilder = new Picasso.Builder(getActivity().getApplicationContext());
 
-                        @Override
-                        public void onError() {
-                            Toast.makeText(getActivity(), "Error loading image.", Toast.LENGTH_LONG).show();
+		// add our custom eat foody request handler (see below for full class)
+		picassoBuilder.addRequestHandler(new DataRequestHandler());
 
-                            finish();
-                        }
-                    });
-        } else if (mImage.startsWith("data:image")) {
+		// Picasso.Builder creates the Picasso object to do the actual requests
+		Picasso picasso = picassoBuilder.build();
 
-            new AsyncTask<Void, Void, File>() {
+		picasso.load(url)
+				.fit()
+				.centerInside()
+				.into(photo, new com.squareup.picasso.Callback() {
+					@Override
+					public void onSuccess() {
+						hideLoadingAndUpdate();
+					}
 
-                protected File doInBackground(Void... params) {
-                    String base64Image = mImage.substring(mImage.indexOf(",") + 1);
-                    return getLocalBitmapFileFromString(base64Image);
-                }
+					@Override
+					public void onError() {
+						Toast.makeText(getActivity(), "Error loading image.", Toast.LENGTH_LONG).show();
 
-                protected void onPostExecute(File file) {
-                    mTempImage = file;
-                    Picasso.with(PhotoActivity.this)
-                            .load(mTempImage)
-                            .fit()
-                            .centerCrop()
-                            .into(photo, new com.squareup.picasso.Callback() {
-                                @Override
-                                public void onSuccess() {
-                                    hideLoadingAndUpdate();
-                                }
+						finish();
+					}
+				});
+	}
 
-                                @Override
-                                public void onError() {
-                                    Toast.makeText(getActivity(), "Error loading image.", Toast.LENGTH_LONG).show();
+	/**
+	 * Create Local Image due to Restrictions
+	 *
+	 * @param imageView
+	 *
+	 * @return
+	 */
+	public Uri getLocalBitmapUri(ImageView imageView) {
+		Drawable drawable = imageView.getDrawable();
+		Bitmap bmp = null;
 
-                                    finish();
-                                }
-                            });
-                }
-            }.execute();
+		if (drawable instanceof BitmapDrawable){
+			bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+		} else {
+			return null;
+		}
 
-        } else {
-            photo.setImageURI(Uri.parse(mImage));
+		// Store image to default external storage directory
+		Uri bmpUri = null;
+		try {
+			File file =  new File(
+					Environment.getExternalStoragePublicDirectory(
+						Environment.DIRECTORY_DOWNLOADS
+					), "share_image_" + System.currentTimeMillis() + ".png");
 
-            hideLoadingAndUpdate();
-        }
-    }
+			file.getParentFile().mkdirs();
 
-    public void onDestroy() {
-        if (mTempImage != null) {
-            mTempImage.delete();
-        }
-        super.onDestroy();
-    }
+			FileOutputStream out = new FileOutputStream(file);
+			bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+			out.close();
+
+			bmpUri = Uri.fromFile(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bmpUri;
+	}
+
+	public class DataRequestHandler extends RequestHandler {
+
+		private static final String DATA_SCHEME = "data";
+
+		@Override
+		public boolean canHandleRequest(Request data) {
+			String scheme = data.uri.getScheme();
+			return scheme.equalsIgnoreCase(DATA_SCHEME);
+		}
+
+		@Override
+		public Result load(Request request, int networkPolicy) throws IOException {
+
+			String uri = request.uri.toString();
+			String imageDataBytes = uri.substring(uri.indexOf(",") + 1);
+			byte[] bytes = Base64.decode(imageDataBytes.getBytes(), Base64.DEFAULT);
+
+			ExifInterface exif = null;
+			try {
+					exif = new ExifInterface(new ByteArrayInputStream(bytes));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+					ExifInterface.ORIENTATION_UNDEFINED);
 
 
-    public File getLocalBitmapFileFromString(String base64) {
-        File file;
-        try {
-            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                    "share_image_" + System.currentTimeMillis() + ".png");
-            file.getParentFile().mkdirs();
-            FileOutputStream output = new FileOutputStream(file);
-            byte[] decoded = Base64.decode(base64, Base64.DEFAULT);
-            output.write(decoded);
-            output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            file = null;
-        }
-        return file;
-    }
+			Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+			if(orientation != 0){
+				bitmap = rotateBitmap(bitmap, orientation);
+			}
+			if (bitmap == null) return null;
 
-    /**
-     * Create Local Image due to Restrictions
-     *
-     * @param imageView
-     * @return
-     */
-    public File getLocalBitmapFileFromView(ImageView imageView) {
-        Drawable drawable = imageView.getDrawable();
-        Bitmap bmp;
+			return new Result(bitmap, Picasso.LoadedFrom.NETWORK);
+		}
+	}
 
-        if (drawable instanceof BitmapDrawable) {
-            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-        } else {
-            return null;
-        }
+	public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
 
-        // Store image to default external storage directory
-        File file;
-        try {
-            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                    "share_image_" + System.currentTimeMillis() + ".png");
-            file.getParentFile().mkdirs();
-            FileOutputStream out = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
-            out.close();
-
-        } catch (IOException e) {
-            file = null;
-            e.printStackTrace();
-        }
-        return file;
-    }
+		Matrix matrix = new Matrix();
+		switch (orientation) {
+			case ExifInterface.ORIENTATION_NORMAL:
+				return bitmap;
+			case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+				matrix.setScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				matrix.setRotate(180);
+				break;
+			case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+				matrix.setRotate(180);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_TRANSPOSE:
+				matrix.setRotate(90);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				matrix.setRotate(90);
+				break;
+			case ExifInterface.ORIENTATION_TRANSVERSE:
+				matrix.setRotate(-90);
+				matrix.postScale(-1, 1);
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				matrix.setRotate(-90);
+				break;
+			default:
+				return bitmap;
+		}
+		try {
+			Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+			bitmap.recycle();
+			return bmRotated;
+		}
+		catch (OutOfMemoryError e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
